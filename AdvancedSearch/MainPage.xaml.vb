d235752f06a73ed05617473d2383f0756c0e55bd
Timothy R. Puffer <timothypuffer@gmail.com>
2012-08-21 11:32:09 -0600

    Intial commit

---
+++ AdvancedSearch/MainPage.xaml.vb
@@ -0,0 +1,68 @@
+﻿Partial Public Class MainPage
+    Inherits UserControl
+
+    Public Sub New()
+        InitializeComponent()
+        lb_ResourceType.ItemsSource = ResourceProvider.GetResourceInfos()
+
+        Dim b As New ComponentModel.BackgroundWorker()
+    End Sub
+
+#Region "Drag and drop"
+
+#End Region
+
+    Private Sub Button_Click(ByVal sender As System.Object, ByVal e As System.Windows.RoutedEventArgs)
+        Dim r As Rectangle = New Rectangle()
+        r.Height = 20
+        r.Width = 30
+        r.Fill = New SolidColorBrush(Colors.White)
+        c_blue.Children.Add(r)
+
+        r.SetValue(Canvas.TopProperty, Convert.ToDouble(10))
+        r.SetValue(Canvas.LeftProperty, Convert.ToDouble(10))
+
+
+        AddHandler r.MouseLeftButtonDown, AddressOf Node_MouseLeftButtonDown
+        AddHandler r.MouseMove, AddressOf Node_MouseMove
+        AddHandler r.MouseLeftButtonUp, AddressOf Node_MouseLeftButtonUp
+
+
+    End Sub
+
+
+    Dim isMouseCaptured As Boolean
+    Dim mouseVerticalPosition As Double
+    Dim mouseHorizontalPosition As Double
+    Private Sub Node_MouseLeftButtonDown(ByVal sender As System.Object, ByVal e As System.Windows.Input.MouseButtonEventArgs)
+        Dim item As UIElement = sender
+        mouseVerticalPosition = e.GetPosition(Nothing).Y
+        mouseHorizontalPosition = e.GetPosition(Nothing).X
+        isMouseCaptured = True
+        item.CaptureMouse()
+    End Sub
+
+    Private Sub Node_MouseMove(ByVal sender As System.Object, ByVal e As System.Windows.Input.MouseEventArgs)
+        Dim item As UIElement = sender
+        If (isMouseCaptured) Then
+            Dim deltaV As Double = e.GetPosition(Nothing).Y - mouseVerticalPosition
+            Dim deltaH As Double = e.GetPosition(Nothing).X - mouseHorizontalPosition
+            Dim newTop As Double = deltaV + item.GetValue(Canvas.TopProperty)
+            Dim newLeft As Double = deltaH + item.GetValue(Canvas.LeftProperty)
+
+            item.SetValue(Canvas.TopProperty, newTop)
+            item.SetValue(Canvas.LeftProperty, newLeft)
+
+            mouseVerticalPosition = e.GetPosition(Nothing).Y
+            mouseHorizontalPosition = e.GetPosition(Nothing).X
+        End If
+    End Sub
+
+    Private Sub Node_MouseLeftButtonUp(ByVal sender As System.Object, ByVal e As System.Windows.Input.MouseButtonEventArgs)
+        Dim item As UIElement = sender
+        isMouseCaptured = False
+        item.ReleaseMouseCapture()
+        mouseVerticalPosition = -1
+        mouseHorizontalPosition = -1
+    End Sub
+End Class
---
+++ AdvancedSearch/ResourceProvider.vb
@@ -0,0 +1,60 @@
+﻿Public Class ResourceProvider
+
+    Enum ResourceType
+        Account
+        Service
+        Member
+    End Enum
+
+    Public Class ResourceInfo
+        Dim _displayName As String
+        Dim _displayObject As Object
+
+        Public Sub New(ByVal displayName As String, ByVal displayObject As Object)
+            _displayName = displayName
+            _displayObject = displayObject
+        End Sub
+
+        Public ReadOnly Property DisplayName() As String
+            Get
+                Return _displayName
+            End Get
+        End Property
+
+        Public ReadOnly Property DisplayObject() As String
+            Get
+                Return _displayObject
+            End Get
+        End Property
+
+    End Class
+
+
+    Shared Function GetResourceInfo(ByVal resourceTypeEnum As ResourceType) As ResourceInfo
+        FillResourceInfo()
+        Dim ret = Nothing
+        If _resourcelookup.TryGetValue(resourceTypeEnum, ret) Then
+            Return ret
+        Else
+            Return Nothing
+        End If
+    End Function
+
+    Shared Function GetResourceInfos() As ResourceInfo()
+        FillResourceInfo()
+        Return _resourcelookup.Values.ToArray
+    End Function
+
+
+    Shared _resourcelookup As Dictionary(Of ResourceType, ResourceInfo)
+    Private Shared Sub FillResourceInfo()
+        If _resourcelookup Is Nothing Then
+            _resourcelookup = New Dictionary(Of ResourceType, ResourceInfo)
+            _resourcelookup.Add(ResourceType.Account, New ResourceInfo("Account", Nothing))
+            _resourcelookup.Add(ResourceType.Service, New ResourceInfo("Service", Nothing))
+            _resourcelookup.Add(ResourceType.Member, New ResourceInfo("Member", Nothing))
+        End If
+    End Sub
+
+
+End Class
