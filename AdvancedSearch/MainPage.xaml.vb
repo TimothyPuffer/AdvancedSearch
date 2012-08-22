fc1b0f46f8605ad4b757490c68f1426f58686177
Timothy R. Puffer <timothypuffer@gmail.com>
2012-08-22 05:11:25 -0600

    moved drag and drop logic to Node | Added NodeSelected Event | Added Allow Move Property to Node

--- AdvancedSearch/MainPage.xaml.vb
+++ AdvancedSearch/MainPage.xaml.vb
@@ -13,56 +13,17 @@
 #End Region
 
     Private Sub Button_Click(ByVal sender As System.Object, ByVal e As System.Windows.RoutedEventArgs)
-        Dim r As Rectangle = New Rectangle()
-        r.Height = 20
-        r.Width = 30
-        r.Fill = New SolidColorBrush(Colors.White)
-        c_blue.Children.Add(r)
+        Dim node As Node_UC = New Node_UC()
+        canvasNodeDisplay.Children.Add(node)
 
-        r.SetValue(Canvas.TopProperty, Convert.ToDouble(10))
-        r.SetValue(Canvas.LeftProperty, Convert.ToDouble(10))
+        node.SetValue(Canvas.TopProperty, Convert.ToDouble(10))
+        node.SetValue(Canvas.LeftProperty, Convert.ToDouble(10))
 
-
-        AddHandler r.MouseLeftButtonDown, AddressOf Node_MouseLeftButtonDown
-        AddHandler r.MouseMove, AddressOf Node_MouseMove
-        AddHandler r.MouseLeftButtonUp, AddressOf Node_MouseLeftButtonUp
-
+        AddHandler node.NodeSelected, AddressOf Node_Selected
 
     End Sub
 
+    Private Sub Node_Selected(ByVal sender As Node_UC)
 
-    Dim isMouseCaptured As Boolean
-    Dim mouseVerticalPosition As Double
-    Dim mouseHorizontalPosition As Double
-    Private Sub Node_MouseLeftButtonDown(ByVal sender As System.Object, ByVal e As System.Windows.Input.MouseButtonEventArgs)
-        Dim item As UIElement = sender
-        mouseVerticalPosition = e.GetPosition(Nothing).Y
-        mouseHorizontalPosition = e.GetPosition(Nothing).X
-        isMouseCaptured = True
-        item.CaptureMouse()
-    End Sub
-
-    Private Sub Node_MouseMove(ByVal sender As System.Object, ByVal e As System.Windows.Input.MouseEventArgs)
-        Dim item As UIElement = sender
-        If (isMouseCaptured) Then
-            Dim deltaV As Double = e.GetPosition(Nothing).Y - mouseVerticalPosition
-            Dim deltaH As Double = e.GetPosition(Nothing).X - mouseHorizontalPosition
-            Dim newTop As Double = deltaV + item.GetValue(Canvas.TopProperty)
-            Dim newLeft As Double = deltaH + item.GetValue(Canvas.LeftProperty)
-
-            item.SetValue(Canvas.TopProperty, newTop)
-            item.SetValue(Canvas.LeftProperty, newLeft)
-
-            mouseVerticalPosition = e.GetPosition(Nothing).Y
-            mouseHorizontalPosition = e.GetPosition(Nothing).X
-        End If
-    End Sub
-
-    Private Sub Node_MouseLeftButtonUp(ByVal sender As System.Object, ByVal e As System.Windows.Input.MouseButtonEventArgs)
-        Dim item As UIElement = sender
-        isMouseCaptured = False
-        item.ReleaseMouseCapture()
-        mouseVerticalPosition = -1
-        mouseHorizontalPosition = -1
     End Sub
 End Class
---
+++ AdvancedSearch/Node_UC.xaml.vb
@@ -0,0 +1,55 @@
+﻿
+Partial Public Class Node_UC
+    Inherits UserControl
+
+    Public Event NodeSelected(ByVal sender As Node_UC)
+
+    Public Property AllowMove As Boolean
+
+    Public Sub New()
+        InitializeComponent()
+        AllowMove = True
+        AddHandler top_part.MouseLeftButtonDown, AddressOf Node_MouseLeftButtonDown
+        AddHandler top_part.MouseMove, AddressOf Node_MouseMove
+        AddHandler top_part.MouseLeftButtonUp, AddressOf Node_MouseLeftButtonUp
+    End Sub
+
+    Dim isMouseCaptured As Boolean
+    Dim mouseVerticalPosition As Double
+    Dim mouseHorizontalPosition As Double
+    Private Sub Node_MouseLeftButtonDown(ByVal sender As System.Object, ByVal e As System.Windows.Input.MouseButtonEventArgs)
+        Dim item As UIElement = sender
+        If (AllowMove) Then
+            mouseVerticalPosition = e.GetPosition(Nothing).Y
+            mouseHorizontalPosition = e.GetPosition(Nothing).X
+            isMouseCaptured = True
+            item.CaptureMouse()
+        End If
+    End Sub
+
+    Private Sub Node_MouseMove(ByVal sender As System.Object, ByVal e As System.Windows.Input.MouseEventArgs)
+        Dim item As UIElement = sender
+        If (isMouseCaptured) Then
+            Dim deltaV As Double = e.GetPosition(Nothing).Y - mouseVerticalPosition
+            Dim deltaH As Double = e.GetPosition(Nothing).X - mouseHorizontalPosition
+            Dim newTop As Double = deltaV + Me.GetValue(Canvas.TopProperty)
+            Dim newLeft As Double = deltaH + Me.GetValue(Canvas.LeftProperty)
+
+            Me.SetValue(Canvas.TopProperty, newTop)
+            Me.SetValue(Canvas.LeftProperty, newLeft)
+
+            mouseVerticalPosition = e.GetPosition(Nothing).Y
+            mouseHorizontalPosition = e.GetPosition(Nothing).X
+        End If
+    End Sub
+
+    Private Sub Node_MouseLeftButtonUp(ByVal sender As System.Object, ByVal e As System.Windows.Input.MouseButtonEventArgs)
+        Dim item As UIElement = sender
+        isMouseCaptured = False
+        item.ReleaseMouseCapture()
+        mouseVerticalPosition = -1
+        mouseHorizontalPosition = -1
+        RaiseEvent NodeSelected(Me)
+    End Sub
+
+End Class
